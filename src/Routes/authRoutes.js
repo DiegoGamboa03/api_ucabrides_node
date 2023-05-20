9bbb1a8d7453a90d16c3003c92fe376a04e1c424
calorenzo21 <celorenzo2104@gmail.com>
2023-05-19 23:58:37 -0400

    se agrego middleware de autenticacion y rutas faltantes en authRoutes

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,10 +1,17 @@
 import express from 'express'
 const router = express.Router();
-import { login, register, register_gmail } from '../Controllers/authControllers.js'
+import { changePassword, login, me, mis_puntos, refresh, register, register_gmail } from '../Controllers/authControllers.js'
+import { verifyAccessToken } from '../helpers/jwt.js';
 
+// Rutas públicas
 router.post('/register', register);
 router.post('/register/gmail', register_gmail)
 router.post('/login', login)
+router.post('/refresh', refresh)
 
+// Rutas protegidas
+router.post('/cambiarclave/:id', verifyAccessToken, changePassword)
+router.get('/me', verifyAccessToken, me)
+router.get('/mis_puntos', verifyAccessToken, mis_puntos)
 
 export default router
