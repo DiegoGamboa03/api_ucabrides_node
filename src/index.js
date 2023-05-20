9501d9e061ac98e0da4e9aa50bd34a313a129f4d
calorenzo21 <celorenzo2104@gmail.com>
2023-05-20 00:11:14 -0400

    se agregaron rutas de vehiculos y error handling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import authRoutes from './Routes/authRoutes.js'
+import vehicleRoutes from './Routes/vehicleRoutes.js'
 import connect from '../config/mongoConnection.js';
+import { routeNotFound, errorHandler } from './helpers/error.js';
 
 const app = express();
 const PORT = process.env.PORT || 8080
@@ -17,6 +19,11 @@ app.use(express.json());
 
 //routes 
 app.use('/api',authRoutes)
+app.use('/api', vehicleRoutes)
+
+//error handlers
+app.use(routeNotFound)
+app.use(errorHandler)
 
 //starting the server
 app.listen(PORT, () => {
